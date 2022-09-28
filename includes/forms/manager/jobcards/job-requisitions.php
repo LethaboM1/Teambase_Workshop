e3774d064175b58d5d48c40f427b913bbb8bdd36
Duncan <duncanm@digitalextreme.co.za>
2022-09-28 10:59:26 +0200

    x

--- includes/forms/manager/jobcards/job-requisitions.php
+++ includes/forms/manager/jobcards/job-requisitions.php
@@ -9,6 +9,7 @@ if (isset($_POST['change_status'])) {
                 $query = "
                         approved_by={$_SESSION['user']['user_id']},
                         approved_by_time='" . date("Y-m-d\TH:i:s") . "',
+                        approved_by_comment='" . htmlentities($_POST['status_comment']) . "'
                         ";
                 break;
 
@@ -16,6 +17,7 @@ if (isset($_POST['change_status'])) {
                 $query = "
                         ordered_by={$_SESSION['user']['user_id']},
                         ordered_by_time='" . date("Y-m-d\TH:i:s") . "',
+                        ordered_by_comment='" . htmlentities($_POST['status_comment']) . "'
                         ";
                 break;
 
@@ -23,6 +25,7 @@ if (isset($_POST['change_status'])) {
                 $query = "
                         received_by={$_SESSION['user']['user_id']},
                         received_by_time='" . date("Y-m-d\TH:i:s") . "',
+                        received_by_comment='" . htmlentities($_POST['status_comment']) . "'
                         ";
                 break;
 
@@ -30,13 +33,32 @@ if (isset($_POST['change_status'])) {
                 $query = "
                         completed_by={$_SESSION['user']['user_id']},
                         completed_by_time='" . date("Y-m-d\TH:i:s") . "',
+                        completed_by_comment='" . htmlentities($_POST['status_comment']) . "'
+                        ";
+                break;
+
+            case 'canceled':
+                $query = "
+                        canceled_by={$_SESSION['user']['user_id']},
+                        canceled_by_time='" . date("Y-m-d\TH:i:s") . "',
+                        canceled_by_comment='" . htmlentities($_POST['status_comment']) . "'
+                        ";
+                break;
+
+            case 'denied':
+                $query = "
+                        denied_by={$_SESSION['user']['user_id']},
+                        denied_by_time='" . date("Y-m-d\TH:i:s") . "',
+                        denied_by_comment='" . htmlentities($_POST['status_comment']) . "'
                         ";
                 break;
         }
+
+
+
         $update_status = dbq("update jobcard_requisitions set
                                     {$query}
-                                    status='{$_POST['status']}',
-                                    status_comment='" . htmlentities($_POST['status_comment']) . "'
+                                    status='{$_POST['status']}'
                                     where request_id={$_POST['request_id']}
                                     ");
         if ($update_status) {
