bc9bf5f6279d603729f1c25224c0932f4c3c5fd5
Duncan <duncanm@digitalextreme.co.za>
2022-09-28 11:25:42 +0200

    x

--- includes/pages/manager/jobcards/list_completed_jobcards.php
+++ includes/pages/manager/jobcards/list_completed_jobcards.php
@@ -79,10 +79,10 @@ $progress = 100;
                                                                 <td><?= $event['total_hours'] ?></td>
                                                                 <td><?= $event['comment'] ?></td>
                                                                 <td class="actions">
-                                                                    <a class="mb-1 mt-1 mr-1 modal-basic" href="#modalViewEvent_<?= $event['event_id'] ?>"><i class="fas fa-pencil-alt"></i></a>
-                                                                    <!-- Modal Edit Event End -->
-                                                                    <!-- Modal Delete -->
-                                                                    <a class="mb-1 mt-1 mr-1 modal-basic" href="#modalDeleteEvent_<?= $event['event_id'] ?>"><i class="far fa-trash-alt"></i></a>
+                                                                    <!--<a class="mb-1 mt-1 mr-1 modal-basic" href="#modalViewEvent_<?= $event['event_id'] ?>"><i class="fas fa-pencil-alt"></i></a>
+                                                                    Modal Edit Event End -->
+                                                                    <!-- Modal Delete 
+                                                                    <a class="mb-1 mt-1 mr-1 modal-basic" href="#modalDeleteEvent_<?= $event['event_id'] ?>"><i class="far fa-trash-alt"></i></a>-->
                                                                     <!-- Modal Delete End -->
                                                                 </td>
                                                             </tr>
