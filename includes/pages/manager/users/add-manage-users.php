743915a745a213288052db82b4cdd652d71c158c
Duncan <duncanm@digitalextreme.co.za>
2022-10-03 08:10:32 +0200

    x

--- includes/pages/manager/users/add-manage-users.php
+++ includes/pages/manager/users/add-manage-users.php
@@ -83,7 +83,7 @@
 					<div class="row">
 						<div class="col-sm-12 col-md-4 pb-sm-3 pb-md-0">
 							<label class="col-form-label" for="formGroupExampleInput">Username</label>
-							<input type="email" name="email" placeholder="Email Address" class="form-control">
+							<input type="username" name="username" placeholder="Email Address" class="form-control">
 						</div>
 						<div class="col-sm-12 col-md-4 pb-sm-3 pb-md-0">
 							<label class="col-form-label" for="formGroupExampleInput">Password</label>
