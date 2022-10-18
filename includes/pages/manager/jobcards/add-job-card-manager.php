ac7764c0f8afc70df1dc587ee8e430a476a82f59
Duncan <duncanm@digitalextreme.co.za>
2022-10-18 19:37:15 +0200

    x

--- includes/pages/manager/jobcards/add-job-card-manager.php
+++ includes/pages/manager/jobcards/add-job-card-manager.php
@@ -67,12 +67,16 @@
 						if ($_SESSION['user']['role'] == 'clerk') {
 							echo inp('clerk_id', '', 'hidden', $_SESSION['user']['user_id']);
 						} else {
-							$get_clerks = dbq("select name, user_id as value from users_tbl where role='clerk'");
+							$get_clerks = dbq("select name, user_id as value, out_of_office from users_tbl where role='clerk'");
 							if ($get_clerks) {
 								$clerk_select_[] = ['name' => 'Select One', 'value' => 0];
 								if (dbr($get_clerks)) {
 									while ($clerk = dbf($get_clerks)) {
-										$clerk_select_[] = $clerk;
+										if ($clerk['out_of_office'] == 1) {
+											$clerk_select_[] = ['name' => $clerk['name'] . " - Out of office", 'value' => $clerk['value']];
+										} else {
+											$clerk_select_[] = ['name' => $clerk['name'], 'value' => $clerk['value']];
+										}
 									}
 								}
 
