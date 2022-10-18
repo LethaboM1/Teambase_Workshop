75332f915728354a55258109df162dbbc7a2b9b8
Duncan <duncanm@digitalextreme.co.za>
2022-10-18 19:16:07 +0200

    x

--- includes/forms/dashboard.php
+++ includes/forms/dashboard.php
@@ -2,7 +2,7 @@
 if ($_POST['outofoffice'] == 'true') {
     msg("Out of office is true");
     $update = dbq("update users_tbl set out_of_office=1 where user_id={$_SESSION['user']['user_id']}");
-    $_SESSION['user']['out_of_office'] = 0;
+    $_SESSION['user']['out_of_office'] = 1;
     go('dashboard.php');
 }
 
