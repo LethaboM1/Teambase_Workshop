7d6100cd43bdbfa318a166767878a0c2cdf451de
Duncan <duncanm@digitalextreme.co.za>
2022-10-18 09:56:53 +0200

    x

--- includes/navigation/menus/manager.php
+++ includes/navigation/menus/manager.php
@@ -1,7 +1,11 @@
 <?php
-
-$get_new_jobs_notify = dbq("select job_id from jobcards where status='logged'");
-$get_job_requests = dbq("select * from jobcard_requisitions where status='requested'");
+if ($_SESSION['user']['role'] == 'clerk') {
+  $get_new_jobs_notify = dbq("select job_id from jobcards where status='logged' and clerk_id={$_SESSION['user']['user_id']}");
+  $get_job_requests = dbq("select * from jobcard_requisitions where status='requested' and clerk_id={$_SESSION['user']['user_id']}");
+} else {
+  $get_new_jobs_notify = dbq("select job_id from jobcards where status='logged'");
+  $get_job_requests = dbq("select * from jobcard_requisitions where status='requested'");
+}
 
 $count_new_jobs = dbr($get_new_jobs_notify);
 $count_new_requests = dbr($get_job_requests);
@@ -9,58 +13,58 @@ $count_new_requests = dbr($get_job_requests);
 $total_notifications = $count_new_jobs + $count_new_requests;
 ?>
 <li>
-    <a href="#" class="dropdown-toggle notification-icon" data-bs-toggle="dropdown">
-        <i class="bx bx-bell"></i>
-        <span class='badge'><?= $total_notifications ?></span>
-    </a>
-    <div class="dropdown-menu notification-menu">
-        <div class="notification-title">
-            <span class="float-end badge badge-default"><?= $total_notifications ?></span>
-            Alerts
-        </div>
+  <a href="#" class="dropdown-toggle notification-icon" data-bs-toggle="dropdown">
+    <i class="bx bx-bell"></i>
+    <span class='badge'><?= $total_notifications ?></span>
+  </a>
+  <div class="dropdown-menu notification-menu">
+    <div class="notification-title">
+      <span class="float-end badge badge-default"><?= $total_notifications ?></span>
+      Alerts
+    </div>
 
-        <div class="content">
-            <ul>
-                <?php
+    <div class="content">
+      <ul>
+        <?php
 
-                if ($count_new_jobs > 0) {
-                ?>
-                    <li>
-                        <a href="dashboard.php?page=new-job" class="clearfix">
-                            <div class="image">
-                                <i class="fas fa-edit bg-danger text-light"></i>
-                            </div>
-                            <span class="title">New Job cards</span>
-                            <span class="message"><?= $count_new_jobs ?> job cards logged</span>
-                        </a>
-                    </li>
-                <?php
-                }
+        if ($count_new_jobs > 0) {
+        ?>
+          <li>
+            <a href="dashboard.php?page=new-job" class="clearfix">
+              <div class="image">
+                <i class="fas fa-edit bg-danger text-light"></i>
+              </div>
+              <span class="title">New Job cards</span>
+              <span class="message"><?= $count_new_jobs ?> job cards logged</span>
+            </a>
+          </li>
+        <?php
+        }
 
-                if ($count_new_requests > 0) {
-                ?>
-                    <li>
-                        <a href="dashboard.php?page=job-requisitions" class="clearfix">
-                            <div class="image">
-                                <i class="fas fa-edit bg-danger text-light"></i>
-                            </div>
-                            <span class="title">New part requests</span>
-                            <span class="message"><?= $count_new_requests ?> requests</span>
-                        </a>
-                    </li>
-                <?php
-                }
-                ?>
-            </ul>
+        if ($count_new_requests > 0) {
+        ?>
+          <li>
+            <a href="dashboard.php?page=job-requisitions" class="clearfix">
+              <div class="image">
+                <i class="fas fa-edit bg-danger text-light"></i>
+              </div>
+              <span class="title">New part requests</span>
+              <span class="message"><?= $count_new_requests ?> requests</span>
+            </a>
+          </li>
+        <?php
+        }
+        ?>
+      </ul>
 
-            <!-- <hr />
+      <!-- <hr />
 
             <div class="text-end">
                 <a href="#" class="view-more">View All</a>
             </div>
             -->
-        </div>
     </div>
+  </div>
 </li>
 
 <?php
