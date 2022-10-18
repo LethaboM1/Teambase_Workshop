d7781f1f0e9a8fba1974dbe478f6a97ecb9814de
Duncan <duncanm@digitalextreme.co.za>
2022-10-18 16:10:04 +0200

    x

--- includes/forms/sms.mechanic.allocated.php
+++ includes/forms/sms.mechanic.allocated.php
@@ -1,20 +1,16 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
-
 $mechanic_ = dbf(dbq("select * from users_tbl where user_id={$_POST['mechanic']}"));
 
-$mail = new PHPMailer(true);
-try {
-	$to = $mechanic_['contact_number'] . "@e-mail2sms.co.za";     //Add a recipient                    
-	//$mail->addReplyTo($_SESSION['user']['email'], $_SESSION['name'] . ' ' . $_SESSION['user']['last_name']);
-	//$mail->addCC($_SESSION['settings']['requisition_mail']);
-	//$mail->isHTML(true);
-	$subject = "#duncanm@digitalextreme.co.za,dx0392781#";
-	$body = "Job card no. {$_POST['jobnumber']} logged for you!";
-	@mail($to, $subject, $body);
-} catch (Exception $e) {
+//$mail = new PHPMailer(true);
+//try {
+$to = $mechanic_['contact_number'] . "@e-mail2sms.co.za";     //Add a recipient                    
+//$mail->addReplyTo($_SESSION['user']['email'], $_SESSION['name'] . ' ' . $_SESSION['user']['last_name']);
+//$mail->addCC($_SESSION['settings']['requisition_mail']);
+//$mail->isHTML(true);
+$subject = "#duncanm@digitalextreme.co.za,dx0392781#";
+$body = "Job card no. {$_POST['jobnumber']} logged for you!";
+@mail($to, $subject, $body);
+/*} catch (Exception $e) {
 	error("SMS could not be sent. Mailer Error: {$mail->ErrorInfo}");
-}
+}*/
