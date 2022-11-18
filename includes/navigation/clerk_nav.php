0346b3673fc8261d2100cbb763b9abefcfadadc4
Duncan <duncanm@digitalextreme.co.za>
2022-11-18 09:56:38 +0200

    x

--- includes/navigation/clerk_nav.php
+++ includes/navigation/clerk_nav.php
@@ -1,12 +1,13 @@
 <?php
 $get_requisitions = dbq("select request_id from jobcard_requisitions where (status!='completed' && status!='rejected'  && status!='canceled') and clerk_id={$_SESSION['user']['user_id']}");
+
 $get_new = dbq("select job_id from jobcards where status='logged' and clerk_id={$_SESSION['user']['user_id']}");
 $get_new_jobnumber = dbq("select job_id from jobcards where status='allocated'");
 $get_open = dbq("select job_id from jobcards where status='open' and clerk_id={$_SESSION['user']['user_id']}");
 $get_completed = dbq("select job_id from jobcards where status='completed' and clerk_id={$_SESSION['user']['user_id']}");
 
-$get_new_requisitions = dbq("select request_id from jobcard_requisitions where status='requested'");
-$get_open_requisitions = dbq("select request_id from jobcard_requisitions where (status!='completed' && status!='rejected' && status!='canceled' && status!='requested')");
+$get_new_requisitions = dbq("select request_id from jobcard_requisitions where status='requested' and clerk_id={$_SESSION['user']['user_id']}");
+$get_open_requisitions = dbq("select request_id from jobcard_requisitions where (status!='completed' && status!='rejected' && status!='canceled' && status!='requested') and clerk_id={$_SESSION['user']['user_id']}");
 
 
 $total_jobs = dbr($get_requisitions) + dbr($get_new) + dbr($get_open) + dbr($get_completed);
