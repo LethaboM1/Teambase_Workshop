40c4adefcae38564a04731de8eb9709c122f0c08
Duncan <duncanm@digitalextreme.co.za>
2023-02-15 15:01:06 +0200

    x

--- includes/pages/manager/jobcards/open-job-cards.php
+++ includes/pages/manager/jobcards/open-job-cards.php
@@ -53,6 +53,26 @@
 		if ($get_jobs) {
 			if (dbr($get_jobs) > 0) {
 				while ($row = dbf($get_jobs)) {
+					$get_last_event = dbq("select * from jobcard_events where job_id={$row['job_id']} order by start_datetime DESC limit 1");
+					if (dbr($get_last_event) > 0) {
+						$chk_event = dbf($get_last_event);
+					} else {
+						$chk_event['start_datetime'] = '';
+					}
+					$row['last_evt_date'] = $chk_event['start_datetime'];
+
+					$jobcards_list[] = $row;
+					//require "./includes/pages/manager/jobcards/list_open_jobcards.php";
+				}
+			}
+
+			if (is_array($jobcards_list) && count($jobcards_list) > 0) {
+				function cmp($a, $b)
+				{
+					return strcmp($b["last_evt_date"], $a["last_evt_date"]);
+				}
+				usort($jobcards_list, 'cmp');
+				foreach ($jobcards_list as $row) {
 					require "./includes/pages/manager/jobcards/list_open_jobcards.php";
 				}
 			} else {
