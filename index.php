5c1cfdf8ad5cd6ef6b04671ad5ff9dcd3a9861bc
Duncan <duncanm@digitalextreme.co.za>
2023-02-16 18:49:03 +0200

    x

--- index.php
+++ index.php
@@ -29,6 +29,7 @@ if (isset($_POST[$field_names['username']], $_POST[$field_names['password']])) {
 
 		dbconn('127.0.0.1', $database_name, $database_user, $database_password);
 		$query = "select * from users_tbl where username='{$_POST[$field_names['username']]}'";
+		error_log($query);
 		$chk_user = dbq($query);
 		if (!$chk_user) error_log("SQL error: " . dbe() . ": {$query}");
 		if (dbr($chk_user) > 0) {
