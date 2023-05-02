21229b57bb3c0031275f2847b7cf4c89f638f41b
Duncan <duncanm@digitalextreme.co.za>
2023-05-02 10:09:42 +0200

    x

--- includes/forms/manager/jobcards/new-defects.php
+++ includes/forms/manager/jobcards/new-defects.php
@@ -1,10 +1,10 @@
 <?php
 if (isset($_POST['report_id'])) {
     if (isset($_POST['reviewed'])) {
-        $update_ = dbq("update ws_defect_reports set status='R', comment='{$_POST['comment']}' where id={$_POST['report_id']}");
+        $update_ = dbq("update ws_defect_reports set status='R', comment='" . htmlentities($_POST['comment'], ENT_QUOTES) . "' where id={$_POST['report_id']}");
         ($update_) ? msg('Report has been reviewed.') : sqlError();
     } else if (isset($_POST['create_jobcard'])) {
-        $update_ = dbq("update ws_defect_reports set comment='{$_POST['comment']}' where id={$_POST['report_id']}");
+        $update_ = dbq("update ws_defect_reports set comment='" . htmlentities($_POST['comment'], ENT_QUOTES) . "' where id={$_POST['report_id']}");
         ($update_) ? go("dashboard.php?page=add-job&defect={$_POST['report_id']}") : sqlError();
     }
 }
